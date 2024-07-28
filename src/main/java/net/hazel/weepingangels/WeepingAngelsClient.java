91d24dd9ae1e85a0199b6eab385fcb14625f60aa
TNTTheLagger <tntcsatornaja@gmail.com>
2024-07-27 19:31:21 -0500

    Initial commit

---
+++ src/main/java/net/hazel/weepingangels/WeepingAngelsClient.java
@@ -0,0 +1,10 @@
+package net.hazel.weepingangels;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeepingAngelsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
